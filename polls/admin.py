91cedf7117d46ca1427e05ffef7cff0e9490458e
aws108 <sergiolizangarcia@gmail.com>
2017-02-14 18:50:25 +0100

    apanyau

---
+++ polls/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from django.contrib import admin
+
+from .models import Blog
+admin.site.register(Blog)
+
+from .models import Category
+admin.site.register(Category)
